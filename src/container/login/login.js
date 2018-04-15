93cb4c492d230a6db73767721ffeac87c7b894f5
CChanggg <373893135@qq.com>
2018-04-15 23:02:48 +0800

    first commit

---
+++ src/container/login/login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../../component/logo/logo'
+class Login extends React.Component{
+    render(){
+        return (
+            <div>
+                <Logo></Logo>
+                <h2>我是登录页</h2>
+            </div>
+        )
+    }
+}
+export default Login
